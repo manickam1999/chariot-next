b1b873ebf54ef7ef1895d1e581c8ecc8ffc34e47
Zi Hang <kevu.lee@gmail.com>
2025-01-02 19:06:24 +0800

    feat(mixpanel): setup base tracking for mixpanel

---
+++ src/app/components/shared/AnalyticsProvider.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Analytics } from "@/app/utils/mixpanel";
+import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
+export function AnalyticsProvider() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (pathname) {
+      Analytics.track("Page View", {
+        path: pathname,
+        search: searchParams.toString(),
+        url: `${pathname}${searchParams.toString() ? `?${searchParams.toString()}` : ""}`,
+      });
+    }
+  }, [pathname, searchParams]);
+
+  return null;
+}
--- src/env.js
+++ src/env.js
@@ -8,9 +8,11 @@ export const env = createEnv({
     NEXT_PUBLIC_USE_MOCK_DATA: z
       .enum(["true", "false"])
       .transform((val) => val === "true"),
+    NEXT_PUBLIC_MIXPANEL_TOKEN: z.string(),
   },
   runtimeEnv: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     NEXT_PUBLIC_USE_MOCK_DATA: process.env.NEXT_PUBLIC_USE_MOCK_DATA,
+    NEXT_PUBLIC_MIXPANEL_TOKEN: process.env.NEXT_PUBLIC_MIXPANEL_TOKEN,
   },
 });
