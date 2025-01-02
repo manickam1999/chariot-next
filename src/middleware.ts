78f45e70894e47e6c6fcd0e4fed566e2f8f655a7
Manickam Murugappan <manickam.murugappan@vitrox.com>
2025-01-02 10:27:19 +0800

    feat(i18n): enhance locale validation and enable locale detection in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,9 @@
 import createMiddleware from 'next-intl/middleware';
 import {routing} from './i18n/routing';
  
-export default createMiddleware(routing);
+export default createMiddleware(routing, {
+  localeDetection: true
+});
  
 export const config = {
   // Match only internationalized pathnames
